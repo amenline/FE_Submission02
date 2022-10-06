109ba596ffd79c0e9dd3900f8056ad2852c668d0
Phronesis <amenline.07@gmail.com>
2022-10-06 05:30:45 +0000

    implemented charts and toggle

--- public/assets/scripts/commonScripts.js
+++ public/assets/scripts/commonScripts.js
@@ -1,11 +1,5 @@
 document.querySelector('body').onload = function () {
-    const refresh_token = getCookie('refresh_token');
-    const access_token = getCookie('access_token');
-    if (refresh_token === '') {
-        window.location.replace('../public/index.html');
-    } else if (access_token === '') {
-        refreshToken(refresh_token);
-    }
+    authenticate();
 };
 
 function logout() {
@@ -13,4 +7,15 @@ function logout() {
     setCookie('access_token', '');
     setCookie('refresh_token', '');
     window.location.replace('../public/index.html');
+}
+
+function authenticate() {
+    console.log('*********runing authenticate')
+    const refresh_token = getCookie('refresh_token');
+    const access_token = getCookie('access_token');
+    if (refresh_token === '') {
+        window.location.replace('../public/index.html');
+    } else if (access_token === '') {
+        refreshToken(refresh_token);
+    }
 }
